35de11731c1f92c46a42f95468c3a60486f796e7
white_rust <x10tion007@gmail.com>
2026-05-19 07:55:04 +0100

    my_vec feature

---
+++ src/macros/my_vec.rs
@@ -0,0 +1,20 @@
+
+
+#[macro_export]
+macro_rules! my_vec {
+    $($elem:expr), *) => {
+        {
+            let mut vec = Vec::new();
+            $(
+                vec.push($elem);
+
+
+            )*
+            vec
+        }
+    };
+
+    () => {
+        Vec::new()
+    };
+}
